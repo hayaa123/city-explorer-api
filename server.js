6b69c0e432223db1f40fbfa175f88c10fc28ffe8
hayaa1234 <hayalawansah@gmail.com>
2021-09-16 17:06:58 +0300

    finish lab 10

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-"use strict";
-const express = require("express");
+'use strict';
 
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const weather = require('./controller/Weather.controller');
 const app = express();
-const cors = require("cors");
-app.use(cors());
-require("dotenv").config();
-const weatherData = require("./data/weather.json");
-const axios=require("axios");
+app.use(cors())
+const movieHandeller = require('./controller/Movie.Controller');
 
-const PORT = process.env.PORT;
-
-const weatherHandle = require('./controller/Weather.controller')
-const movieHandeller = require('./controller/Movie.Controller')
-
-app.get("/weather",weatherHandle);
+app.get('/weather', weatherHandler);
 app.get("/movie", movieHandeller);
 
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
 
+function weatherHandler(request, response) {
+
+  let lat =Number(request.query.lat);
+  let lon  = Number(request.query.lon);
+  weather(lat, lon)
+  .then(summaries => response.send(summaries))
+  .catch((error) => {
+    console.error(error);
+    response.status(200).send('Sorry. Something went wrong!')
+  });
+}  
+
+app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
